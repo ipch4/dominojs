eafa555697fc56e60bc6e07045293ed18e7fd8cb
ipch4 <35901516+ipch4@users.noreply.github.com>
2019-07-15 15:23:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,4 @@
 
-
-
-// Projet Domino “Cochon!”
-// L’idée est de créer une application de jeu de dominos sur mobile.
-// Il y aura la possibilité de choisir entre le jeu Martiniquais ou Guadeloupéen, et d’ajouter ou pas l’option Parisien.
-// Graphiquement, à chaque défaite, un attribut du cochon sera ajouté au joueur qui perd (oreilles, queue, groin…), et au bout de 3 défaites, le joueur deviendra un cochon.
-//
-//  1. MVP Minimum Valuable Project
-//
-// - Connexion
-// - Sessions
-// - Algorithme classique du jeu de dominos
-//
-//
-// 2. Outils
-//
-// Nous devons faire un choix de framework adapté, de modèle d’architecture, de technologie, via un benchmark.
-//
-// - Progressive Wep Apps
-// - Framework hybride : utilisation des fonctionnalités du téléphone
-// - Ionic
-
-
-
-
     window.game = {};
 
     window.game.plateau = [
